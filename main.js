291d7c096356d8bdb302ecd1d5fdfb6297219808
Siya117 <78736374+Siya117@users.noreply.github.com>
2021-04-13 11:35:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function showparagraph1(){
+    var input1 = [];
+    for (var j = 1; j <= 4; j++) 
+    {
+        input.push(document.getElementById("para1_text_input"+j).value;);]
+        document.getElementById("para1")inner.html = input.join(". ");
+    }
+
+}
+
+function showparagraph2(){
+    var input2 = [];
+    for (var j = 1; j <= 4; j++) 
+    {
+        input.push(document.getElementById("para2_text_input"+j).value;);]
+        document.getElementById("para1")inner.html = input.join(". ");
+    }
+
+}
